cf53a5aba08d069c9e30c648b880d486d39417d5
sinapad <malinoski.iuri@gmail.com>
2019-07-19 14:35:08 -0300

    python web service skeleton, plus sample

---
+++ pythonwebserver/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/iuri/git/exercises-angularwebclient-pythonwebserver/pythonwebserver/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
