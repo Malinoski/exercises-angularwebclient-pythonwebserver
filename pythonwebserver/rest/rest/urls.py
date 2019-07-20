b0814554789141c50cf26d31e16870c43cb7b257
sinapad <malinoski.iuri@gmail.com>
2019-07-20 13:49:28 -0300

    add home page to admin

--- pythonwebserver/rest/rest/urls.py
+++ pythonwebserver/rest/rest/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', admin.site.urls),
     path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
     path('quickstart/', include('quickstart.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
