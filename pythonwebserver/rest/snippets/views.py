0e11e92f460c2f14af83187d3b2b8f14c93f8e34
sinapad <malinoski.iuri@gmail.com>
2019-07-23 11:59:33 -0300

    Web server updated to use class based view with automatic operation, like list/create/update for http request, like get/post/put

--- pythonwebserver/rest/snippets/views.py
+++ pythonwebserver/rest/snippets/views.py
@@ -1,65 +1,95 @@
 from .models import Snippet
 from .serializers import SnippetSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import mixins
+from rest_framework import generics
 
-
-# # Function based views
+# # Function based views with implicit use of JSON and HTTP response code
+"""
 # @csrf_exempt # Note: @csrf_exempt is used to avoid attacks (clients that won't have a CSRF token):
-# @api_view(['GET', 'POST'])
-# def snippet_list(request, format=None):
-# # Class based view
-class SnippetList(APIView):
-    """
-    List all code snippets, or create a new snippet.
-    """
-    # if request.method == 'GET':
-    def get(self, request, format=None):
+def snippet_list(request):
+    # List all code snippets, or create a new snippet.    
+    if request.method == 'GET':
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
-        # return JsonResponse(serializer.data, safe=False)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+"""
+
+# # Function based views with object response for data and HTTP response
+"""
+@api_view(['GET', 'POST'])
+def snippet_list(request):
+    
+    # List all code snippets, or create a new snippet.    
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
         return Response(serializer.data)
 
-    # elif request.method == 'POST':
-    def post(self, request, format=None):
-        # data = JSONParser().parse(request)
-        # serializer = SnippetSerializer(data=data)
+    elif request.method == 'POST':
         serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            # return JsonResponse(serializer.data, status=201)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # return JsonResponse(serializer.errors, status=400)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+"""
 
-
-# # Class based view
-class SnippetDetail(APIView):
-    """
-    Retrieve, update or delete a snippet instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Snippet.objects.get(pk=pk)
-        except Snippet.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet)
+# # Class based view with manual list generation for HTTP response
+"""
+# # Class based view (https://www.django-rest-framework.org/tutorial/2-requests-and-responses/)
+class SnippetList(APIView):
+    
+    # List all snippets, or create a new snippet.
+    def get(self, request, format=None):
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
         return Response(serializer.data)
 
-    def put(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet, data=request.data)
+    def post(self, request, format=None):
+        serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+"""
 
-    def delete(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# # Class based view with automatic operation, like list/create/update for http request, like get/post/put
+# # (https://www.django-rest-framework.org/tutorial/3-class-based-views/)
+class SnippetList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+# # Class based view with automatic operation, like list/create/update for http request, like get/post/put
+# # (https://www.django-rest-framework.org/tutorial/3-class-based-views/)
+class SnippetDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
